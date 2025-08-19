de803cfa5b8de1ed6431e34dec83159aee544c4a
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-08-19 18:28:47 +0530

    Created onboard screen 3

--- app/src/main/java/com/example/coincircle/Onboarding2Activity.kt
+++ app/src/main/java/com/example/coincircle/Onboarding2Activity.kt
@@ -1,6 +1,8 @@
 package com.example.coincircle
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,10 @@ class Onboarding2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding2)
 
-
+        val nextPageBtn = findViewById<Button>(R.id.onboarding2Btn)
+        nextPageBtn.setOnClickListener {
+            val intent = Intent(this, Onboarding2Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
