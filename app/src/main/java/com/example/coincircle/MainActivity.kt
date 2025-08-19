d47230431f6533b69339062a63b8d6a18b5495a7
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-08-19 12:00:27 +0530

    Clear MainActivity file

--- app/src/main/java/com/example/coincircle/MainActivity.kt
+++ app/src/main/java/com/example/coincircle/MainActivity.kt
@@ -16,32 +16,7 @@ import com.example.coincircle.ui.theme.CoinCircleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CoinCircleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoinCircleTheme {
-        Greeting("Android")
     }
 }
