961ed81a1859772c1268afd961e68b9303c7e3ca
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-08-19 11:06:39 +0530

    Change compileSdk & targetSdk into 35 to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.coincircle"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.coincircle"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
