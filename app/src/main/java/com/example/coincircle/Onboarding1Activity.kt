8109941a4457385625e3135ca608642ad484815d
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-08-19 18:15:52 +0530

    Created onboard screen 2

--- app/src/main/java/com/example/coincircle/Onboarding1Activity.kt
+++ app/src/main/java/com/example/coincircle/Onboarding1Activity.kt
@@ -1,11 +1,19 @@
 package com.example.coincircle
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Onboarding1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding1)
+
+        val nextPageBtn = findViewById<Button>(R.id.onboarding1Btn)
+        nextPageBtn.setOnClickListener {
+            val intent = Intent(this, Onboarding2Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
