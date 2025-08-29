ee795c36768c25adb6663b9d647c12f2de5047f7
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-08-29 09:39:28 +0530

    Created card added page

--- app/src/main/java/com/example/coincircle/CardAddedActivity.kt
+++ app/src/main/java/com/example/coincircle/CardAddedActivity.kt
@@ -10,22 +10,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LinkCardActivity : AppCompatActivity() {
+class CardAddedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_link_card)
+        setContentView(R.layout.activity_card_added)
 
         val backBtn = findViewById<ImageView>(R.id.backBtn)
         backBtn.setOnClickListener {
-            val intent = Intent(this, CardDetailsActivity::class.java)
+            val intent = Intent(this, LinkCardActivity::class.java)
             finish()
-        }
-
-        val continueBtn = findViewById<Button>(R.id.continueBtn)
-        continueBtn.setOnClickListener {
-            val intent = Intent(this, CardAddedActivity::class.java)
-            startActivity(intent)
         }
     }
 }
